cc9b4f6adbb8599bd7ea1dca46ad7894343d4966
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-05-29 19:27:09 -0700

    test

--- source/interpPage.js
+++ source/interpPage.js
@@ -1,3 +1,5 @@
+const { range } = require("express/lib/request");
+
 // Flip cards
 const cards = document.querySelectorAll(".card");
 
@@ -23,3 +25,11 @@ expandButton.addEventListener('click', () => {
     });
   }
 });
+
+const tempNum = [1, 2, 3];
+const interp = JSON.parse('{"Name": "Card 1", "Interp1": "dummy text1", "Interp2": "dummy text2",  "Interp3": "dummy text3"}, { "Name": "Card 2", "Interp1": "dummy text4", "Interp2": "dummy text5", "Interp3": "dummy text6"}, {"Name": "Card 3", "Interp1": "dummy text7", "Interp2": "dummy text8", "Interp3": "dummy text9"}')
+for (const card of cards) {
+  let index = Math.floor(Math.random() * interp.size) - 1;
+  card.querySelector('h3').innerText = interp[0]["Card" + tempNum[index]];
+  tempNum.splice(index, 1);
+}

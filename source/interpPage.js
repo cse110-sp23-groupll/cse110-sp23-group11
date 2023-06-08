22a5bf12ee3340db6d659c79c6339303507a1653
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2023-06-08 15:25:55 -0700

    Fixing code style and making adjustments to interpretation so it doesn't randomly generate each time on refresh

--- source/interpPage.js
+++ source/interpPage.js
@@ -1,73 +1,10 @@
-// Flip cards
-import cardData from "./cards.json" assert { type: 'json' }
-import interpData from "./interpretations.json" assert { type: 'json'}
+import cardData from "./cards.json" assert { type: 'json' } // Card json information
+import interpData from "./interpretations.json" assert { type: 'json'} // Overall interpretation JSON information
 
 const cards = document.querySelectorAll(".card");
 
 setCardProp();
 
-
-/**
- * Pulls JSON card strings from localStorage and parses them for use in updating card information
- */
-function getCardNamesFromLS() {
-  let selectedCards = [localStorage.getItem("past"), localStorage.getItem("present"), localStorage.getItem("future")];
-  let names = [];
-  
-  for (const card of selectedCards) {
-    const cardJSON = JSON.parse(card);
-    if (cardJSON["reversed"]) {
-      names.push(cardJSON["Name"] + " (Reversed)")
-    } else {
-      names.push(cardJSON["Name"]);
-    }
-  }
-
-  return names;
-}
-
-/**
- * Sets card information using past, present, and future information
- */
-function setCardProp() {
-  let cardsSelected = getCardNamesFromLS();
-  let finalInterp = document.querySelector('h2');
-
-  for (let i = 0; i < cards.length; i++) {
-    const name = cardsSelected[i];
-    const cardImg = cards[i].querySelector('img');
-    const backCard = cards[i].querySelector('.card-back');
-    cards[i].querySelector('h3').innerText = name;
-    cardImg.src = cardData[0][name]["Source"]
-    cardImg.alt = "Image for " + name;
-    backCard.querySelector('h3').innerText = name;
-    backCard.querySelector('h4').innerText = cardData[0][name]["Description"];
-    backCard.querySelector('p').innerText = cardData[0][name]["Interpretation"];
-  }
-
-  finalInterp.innerText = randomInterp(cardsSelected);
-  
-}
-
-/**
- * Returns a random interpretation given the values of the cards which determine how positive/negative a fortune will be
- * @param {*} cardNames A list of the names of the cards chosen by the user
- * @returns A random interpretation given the sum of the values
- */
-
-function randomInterp(cardNames) {
-  let total = 0;
-  for (const name of cardNames) {
-    total += cardData[0][name]["Value"];
-  }
-
-  let randomChoice = Math.floor(Math.random() * 5) + 1;
-  return interpData[0][total.toString()]["Interpretation" + randomChoice];
-}
-
-function flipCard() {
-  this.classList.toggle("is-flipped");
-}
 cards.forEach(card => card.addEventListener("click", flipCard));
 
 // Exit page button redirects to the welcome page
@@ -101,3 +38,87 @@ expandButton.addEventListener('click', () => {
     });
   }
 });
+
+/**
+ * Pulls JSON card strings from localStorage and parses them for use in updating card information
+ * @returns {Array<String>} names of cards selected by the user
+ */
+function getCardNamesFromLS() {
+  let selectedCards = [localStorage.getItem("past"), localStorage.getItem("present"), localStorage.getItem("future")];
+  let names = [];
+  
+  for (const card of selectedCards) {
+    const cardJSON = JSON.parse(card);
+    if (cardJSON["reversed"]) {
+      names.push(cardJSON["Name"] + " (Reversed)")
+    } else {
+      names.push(cardJSON["Name"]);
+    }
+  }
+
+  return names;
+}
+
+/**
+ * Sets card information using past, present, and future information
+ */
+function setCardProp() {
+  let cardsSelected = getCardNamesFromLS();
+  let finalInterp = document.querySelector('h2');
+
+  for (let i = 0; i < cards.length; i++) {
+    const name = cardsSelected[i];
+
+    const cardImg = cards[i].querySelector('img');
+    const backCard = cards[i].querySelector('.card-back');
+
+    // Sets the image and name for the front side of the card
+    cards[i].querySelector('h3').innerText = name;
+    cardImg.src = cardData[0][name]["Source"]
+    cardImg.alt = "Image for " + name;
+
+    // Sets information of the back side of the card
+    backCard.querySelector('h3').innerText = name;
+    backCard.querySelector('h4').innerText = cardData[0][name]["Description"];
+    backCard.querySelector('p').innerText = cardData[0][name]["Interpretation"];
+  }
+
+  // Overall interpretation randomized using the values of the cards selected
+  // If page refreshed (meaning localStorage info is as is), reuse old interpretation
+  if (localStorage.getItem("interpretation") == null) {
+    finalInterp.innerText = randomInterp(cardsSelected);
+  } else {
+    finalInterp.innerText = localStorage.getItem("interpretation");
+  }
+}
+
+/**
+ * Returns a random interpretation given the values of the cards which determine how positive/negative a fortune will be
+ * @param {Array<String>} cardNames A list of the names of the cards chosen by the user
+ * @returns {String} A random interpretation given the sum of the values
+ */
+
+function randomInterp(cardNames) {
+  let total = 0;
+  
+  // Sums up the card values
+  for (const name of cardNames) {
+    total += cardData[0][name]["Value"];
+  }
+
+  // Picks one of 5 possible interpretations for that sum combination
+  let randomChoice = Math.floor(Math.random() * 5) + 1;
+
+  // Store interpretation in local storage for later use
+  let interpretation = interpData[0][total.toString()]["Interpretation" + randomChoice];
+  localStorage.setItem("interpretation", interpretation)
+
+  return interpretation;
+}
+
+/**
+ * Flips the card to reveal the other side with the relevant information (front side for picture and name, back side for description and interpretation info)
+ */
+function flipCard() {
+  this.classList.toggle("is-flipped");
+}
